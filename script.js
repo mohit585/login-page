5e27ea89b1c6033fdeebf661e624b3b6d5919f57
mohit kumar <mk0765361@gmail.com>
2026-03-28 00:25:27 +0530

    Added my project files

---
+++ script.js
@@ -0,0 +1,8 @@
+function showLogin() {
+  document.getElementById("signupForm").classList.remove("hidden");
+  document.getElementById("loginForm").classList.add("hidden");
+}
+function showSignup(){
+  document.getElementById("loginForm").classList.remove("hidden");
+  document.getElementById("signupForm").classList.add("hidden");
+}
